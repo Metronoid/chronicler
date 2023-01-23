647daf260447465c128d5466c523ada09088ca6e
Wander <wander@veranto.nl>
2023-01-23 17:06:31 +0100

    Split the categories of merits

--- app/merits/layout.tsx
+++ app/merits/layout.tsx
@@ -1,23 +1,9 @@
 import { TabGroup } from '#/ui/TabGroup';
 import React from 'react';
 import lodash from 'lodash'
-import glob from 'fast-glob';
-
-function getMeritCategories() {
-  const cats: string[] = [];
-  const categories = glob.sync('content/merits/**', {onlyDirectories: true});
-  for (const category of categories) {
-    const files = glob.sync(`${category}/*.(mdx|md)`);
-    if(files.length > 0){
-      const spl = category.split('/');
-      cats.push(spl[spl.length-1])
-    }
-  }
-  return cats;
-}
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
-  const categories = getMeritCategories();
+  const categories = ['mental', 'physical', 'social', 'supernatural', 'fighting'];
   return (
     <div className="space-y-9">
       <TabGroup
